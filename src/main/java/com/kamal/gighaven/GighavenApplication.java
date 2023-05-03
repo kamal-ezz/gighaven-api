4959232fbb89128319544f7ddc910d00946d8f7b
kamal-ezz <kamal.ezzarmou1@gmail.com>
2023-05-03 02:40:01 +0100

    first commit

---
+++ src/main/java/com/kamal/gighaven/GighavenApplication.java
@@ -0,0 +1,13 @@
+package com.kamal.gighaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GighavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GighavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamal/gighaven/GighavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamal.gighaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GighavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
